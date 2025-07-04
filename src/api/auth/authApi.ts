e74cb892e29905ea0491710844fff3c44833c129
shimhanmohamad <mohamadshimhan@gmail.com>
2025-07-04 11:28:16 +0530

    auth

--- src/api/auth/authApi.ts
+++ src/api/auth/authApi.ts
@@ -2,19 +2,16 @@
 import apiClient from "../apiClient.ts";
 import axios from "axios";
 
-export const login = async (credentials: {
-  email: string;
-  password: string;
-}) => {
+export const login = async (credentials: { email: string; password: string }) => {
   try {
     console.log(credentials);
-    const response = await apiClient.post("v1/auth/login", credentials);
+    const response = await apiClient.post('/auth/login', credentials);
     return response.data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
-      throw error.response?.data?.message || "Login failed";
+      throw error.response?.data?.message || 'Login failed';
     }
-    throw "An unexpected error occurred";
+    throw 'An unexpected error occurred';
   }
 };
 
