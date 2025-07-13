9df9b05a380e3075f79515bee6077528ad1ae5e2
Risafamtii <risafaimtiyas@gmail.com>
2025-07-13 16:01:19 +0530

    Add Sigiriya image to assets and update images export

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -29,7 +29,9 @@ import Train from './Landing/train.jpg';
 import Lake from './Landing/PanoramicRiver.jpg';
 import Church from './Landing/Church.jpg';
 import Kovil from './Landing/Kovil.jpg';
-import TourCreate from './Tour/view-travel-adventure-essentials.jpg'
+import TourCreate from './Tour/view-travel-adventure-essentials.jpg';
+import Sigiriya from './Landing/sigiria.jpg';
+
 
 
 export const images = {
@@ -60,6 +62,7 @@ export const images = {
   Mosque,
   Evening,
   Train,
+  Sigiriya,
   Lake,
   Church,
   Kovil,
